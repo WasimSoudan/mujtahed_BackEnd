858425dbd911412adb04b3c9b9654db3a0d6a0e0
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2024-08-28 10:57:53 +0300

    Add Shiftings

---
+++ src/Modules/shifting/Shiftings/Delete/index.js
@@ -0,0 +1,20 @@
+module.exports =
+ route =>
+ ({ pool, ...app }) => {
+  // Delete Shiftings
+  app.delete(route, async (req, res) => {
+   const { isPositiveInteger } = res.locals.utils;
+
+   try {
+    const { id } = req.query;
+
+    if (!isPositiveInteger(id)) return res.status(404).json({ success: false, message: 'Shifting was not found.' });
+
+    const { rows } = await pool.query('DELETE FROM shifting."Shiftings" WHERE 1=1 AND id = $1 RETURNING *', [id]);
+
+    res.json({ Success: true, msg: 'shifting was deleted successfully.', data: rows });
+   } catch ({ message }) {
+    res.json({ success: false, message });
+   }
+  });
+ };
