9e3bdf44fec74ecd918e16f62b8df80e1fab032b
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2024-08-28 09:54:29 +0300

    Add incomings

--- src/Modules/index.js
+++ src/Modules/index.js
@@ -2,4 +2,5 @@ module.exports = app => {
  ['Phone', 'Info', 'Vehicles', 'Driver_Phones'].forEach(d => require('./driver/' + d)(app));
  ['Container_Sizes'].forEach(d => require('./container/' + d)(app));
  ['Agents', 'Destination_To'].forEach(d => require('./shifting/' + d)(app));
+ ['Incomings'].forEach(d => require('./inout/' + d)(app));
 };
